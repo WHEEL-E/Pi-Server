3098defe356b00e8bc01becf6538e668e15f4855
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2022-06-07 04:18:53 +0200

    init project + socket connection done

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// Add messages when sockets open and close connections
+io.on('connection', socket => {
+  console.log(`[${socket.id}] socket connected`);
+  socket.on('disconnect', reason => {
+    console.log(`[${socket.id}] socket disconnected - ${reason}`);
+  });
+
+  socket.on("handShake", msg => { console.log(msg) });
+
+  socket.on("action", action => { console.log(action) })
+
+  socket.on("action-stop", stop => { console.log(stop) })
+});
+
+// // Broadcast the current server time as global message, every 1s
+// setInterval(() => {
+//   io.sockets.emit('time-msg', { time: new Date().toISOString() });
+// }, 1000);
+
+// Show the index.html by default
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+// Start the express server
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
