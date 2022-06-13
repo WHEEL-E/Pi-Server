b925cdb89c0745bdaf95b9214ad149769f3435ac
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2022-06-14 01:08:43 +0200

    testing port

--- UARTFunctions.js
+++ UARTFunctions.js
@@ -1,14 +1,22 @@
-const { SerialPort } = require('serialport')
+const { SerialPort } = require('serialport');
 
-const port = new SerialPort({
-    port: "/dev/ttyS0",
-    baudrate: 115200,
-    parity: serial.PARITY_NONE,
-    stopbits: serial.STOPBITS_ONE,
-    bytesize: serial.EIGHTBITS,
-    write_timeout: 1,
-})
+let port
 
-export const executeAction = (action) => {
+exports.initPort = () => {
+    port = new SerialPort({
+        path: "/dev/ttyS0",
+        baudRate: 115200,
+        parity: SerialPort.PARITY_NONE,
+        stopbits: SerialPort.STOPBITS_ONE,
+        bytesize: SerialPort.EIGHTBITS,
+        write_timeout: 1.0,
+    });
+
+    console.log("Port initialized", port);
+}
+
+
+exports.executeAction = (action) => {
+    console.log("Executing action:", action);
     port.write(action)
 }
--- index.js
+++ index.js
@@ -3,53 +3,53 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const cors = require('cors')
 const { spawn } = require("child_process");
+const { executeAction, initPort } = require('./UARTFunctions');
 
 io.on('connection', socket => {
   console.log(`[${socket.id}] socket connected`);
-  runCommand("ls", ["-l"]);
+  initPort()
+
   socket.on('disconnect', reason => {
     stopCommand()
     console.log(`[${socket.id}] socket disconnected - ${reason}`);
   });
 
-  socket.on("handShake", msg => { console.log(msg) });
-  
-  socket.on("action", action => { console.log(action); })
-  
-  socket.on("action-stop", stop => { console.log(stop) })
+  socket.on("action", action => { executeAction(action) })
+
+  socket.on("action-stop", stop => { executeAction(stop) })
 });
 
-let command
 
-const runCommand = (command, args) => {
-  command = spawn(command, args ? args : []); // (command, argument to the command)
-  
-  command.stdout.on("data", data => {
-    console.log(`stdout: ${data}`);
-  });
-  
-  command.stderr.on("data", data => {
-    console.log(`stderr: ${data}`);
-  });
-  
-  command.on('error', (error) => {
-    console.log(`error: ${error.message}`);
-  });
-  
-  command.on("close", code => {
-    console.log(`child process exited with code ${code}`);
-  });
-}
+// TODO Do we still need this command thing ?! 
+// let command
 
-const stopCommand = () => {
-  if (command) {
-    command.kill()
-  }
-}
+// const runCommand = (command, args) => {
+//   command = spawn(command, args ? args : []); // (command, argument to the command)
+
+//   command.stdout.on("data", data => {
+//     console.log(`stdout: ${data}`);
+//   });
+
+//   command.stderr.on("data", data => {
+//     console.log(`stderr: ${data}`);
+//   });
+
+//   command.on('error', (error) => {
+//     console.log(`error: ${error.message}`);
+//   });
+
+//   command.on("close", code => {
+//     console.log(`child process exited with code ${code}`);
+//   });
+// }
+
+// const stopCommand = () => {
+//   if (command) {
+//     command.kill()
+//   }
+// }
 
 app.use(cors());
-
-runCommand("ls", ["-l"]);
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
